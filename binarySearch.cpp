c7ced613e01a2b00c5e727af398df92b803788b4
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:03:37 +0700

    menambahkan fungsi binary pertama

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -66,3 +66,18 @@ void display()
     cout << endl;
 }
 
+void BinarySearch()
+{
+    cout << "\n===============================================\n";
+    cout << "       Elemen Array Setelah Diurutkan (Asc)       \n";
+    cout << "==================================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << "-->";
+        }
+    }
+    cout << endl;
+}
